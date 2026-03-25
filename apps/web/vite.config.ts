018a0ed4b0435e7c711c5f61d3437d2608751608
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2026-03-25 08:48:17 -0500

    build: add cloudflare plugin

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,3 +1,4 @@
+import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
@@ -6,15 +7,16 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	plugins: [
-		tsconfigPaths(),
-		tailwindcss(),
-		tanstackStart(),
-		nitro(),
-		viteReact(),
-	],
-	server: {
-		port: 3000,
-	},
-	nitro: {},
+  nitro: {},
+  plugins: [
+    tsconfigPaths(),
+    tailwindcss(),
+    cloudflare({ viteEnvironment: { name: "ssr" } }),
+    tanstackStart(),
+    nitro(),
+    viteReact(),
+  ],
+  server: {
+    port: 3000,
+  },
 });
