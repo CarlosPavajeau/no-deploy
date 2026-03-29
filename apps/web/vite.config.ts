f39ace51f4284b46b9e45b38a4ece408c72679a6
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2026-03-29 17:58:16 -0500

    revert plugin configuration

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,24 +1,16 @@
+import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
-import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  nitro: {
-    cloudflare: {
-      deployConfig: true,
-      nodeCompat: true,
-    },
-    compatibilityDate: "2024-09-19",
-    preset: "cloudflare_module",
-  },
   plugins: [
     tsconfigPaths(),
     tailwindcss(),
+    cloudflare({ viteEnvironment: { name: "ssr" } }),
     tanstackStart(),
-    nitro(),
     viteReact(),
   ],
   server: {
